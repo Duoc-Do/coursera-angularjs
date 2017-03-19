20fc9606d5d93a8a14cf55ac179bdb5dd88ae863
Duoc-Do <duocdo.it@gmail.com>
2017-03-20 06:22:50 +0700

    'minification'

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -1,1 +1,1 @@
-!function(){"use strict";function e(e){e.name="DuocDo",e.stateOfBeing="smiley",e.sayMessage=function(){return"Have a nice day!"},e.feedDuocDo=function(){e.stateOfBeing="sad"}}angular.module("LunchCheck",[]).controller("LunchCheckController",e),e.$inspect=["$scope"]}();
+!function(){"use strict";function e(e){e.name="DuocDo",e.stateOfBeing="smiley",e.sayMessage=function(){return"Have a nice day!"},e.feedDuocDo=function(){e.stateOfBeing="sad"}}angular.module("LunchCheck",[]).controller("LunchCheckController",["$scope",e]),e.$inspect=["$scope"]}();
