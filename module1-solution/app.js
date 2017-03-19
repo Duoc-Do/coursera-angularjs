bed606048cc1a4a27385c3d8574af568d374421b
Duoc-Do <duocdo.it@gmail.com>
2017-03-20 06:05:49 +0700

    'module1-solution'

---
+++ module1-solution/app.js
@@ -0,0 +1,1 @@
+!function(){"use strict";function e(e){e.name="DuocDo",e.stateOfBeing="smiley",e.sayMessage=function(){return"Have a nice day!"},e.feedDuocDo=function(){e.stateOfBeing="sad"}}angular.module("LunchCheck",[]).controller("LunchCheckController",e),e.$inspect=["$scope"]}();
