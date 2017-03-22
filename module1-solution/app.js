68c4898da259c7faad00576a60ebbbb7d7b8d22f
Duoc-Do <duocdo.it@gmail.com>
2017-03-23 06:16:05 +0700

    updated Minify my JavaScript

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -1,35 +1,13 @@
-(function() {
-	'use strict';
-	angular.module('LunchCheck', [])
-		.controller('LunchCheckController', LunchCheckController);
-	LunchCheckController.$inject = ['$scope', '$filter'];
+! function() {
+	"use strict";
 
-	function LunchCheckController($scope, $filter) {
-		$scope.name = "";
-		$scope.message = "";
-		$scope.result = "";
-		$scope.btnCheck = function() {
-			var items = $scope.getItems($scope.name);
-			if (items.length === 0) {
-				$scope.result = "Please enter data first";
-				document.getElementById('txtResult').style.color = '#ff2400';
-				document.getElementById('txtName').style.borderColor = '#ff2400';
-			} else if (items.length > 3) {
-				$scope.result = "Too much!";
-				document.getElementById('txtResult').style.color = '#05b626';
-				document.getElementById('txtName').style.borderColor = '#05b626';
-			} else {
-				$scope.result = "Enjoy";
-				document.getElementById('txtResult').style.color = '#05b626';
-				document.getElementById('txtName').style.borderColor = '#05b626';
-			};
-			$scope.message = $scope.result;
-		}
-		$scope.getItems = function(item) {
-			if (typeof item !== "string" || item === "") {
-				return [];
-			}
-			return item.split(",");
+	function e(e, t) {
+		e.name = "", e.message = "", e.result = "", e.btnCheck = function() {
+			var t = e.getItems(e.name);
+			0 === t.length ? (e.result = "Please enter data first", document.getElementById("txtResult").style.color = "#ff2400", document.getElementById("txtName").style.borderColor = "#ff2400") : t.length > 3 ? (e.result = "Too much!", document.getElementById("txtResult").style.color = "#05b626", document.getElementById("txtName").style.borderColor = "#05b626") : (e.result = "Enjoy", document.getElementById("txtResult").style.color = "#05b626", document.getElementById("txtName").style.borderColor = "#05b626"), e.message = e.result
+		}, e.getItems = function(e) {
+			return "string" != typeof e || "" === e ? [] : e.split(",")
 		}
 	}
-})();
+	angular.module("LunchCheck", []).controller("LunchCheckController", e), e.$inject = ["$scope", "$filter"]
+}();
