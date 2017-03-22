16e460ed11d4d933f2738d70375a16d9c67a6909
Duoc-Do <duocdo.it@gmail.com>
2017-03-23 05:45:29 +0700

    updated Module 1 Coding Assignment again

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -1,1 +1,35 @@
-!function(){"use strict";function e(e){e.name="DuocDo",e.stateOfBeing="smiley",e.sayMessage=function(){return"Have a nice day!"},e.feedDuocDo=function(){e.stateOfBeing="sad"}}angular.module("LunchCheck",[]).controller("LunchCheckController",["$scope",e]),e.$inspect=["$scope"]}();
+(function() {
+	'use strict';
+	angular.module('LunchCheck', [])
+		.controller('LunchCheckController', LunchCheckController);
+	LunchCheckController.$inject = ['$scope', '$filter'];
+
+	function LunchCheckController($scope, $filter) {
+		$scope.name = "";
+		$scope.message = "";
+		$scope.result = "";
+		$scope.btnCheck = function() {
+			var items = $scope.getItems($scope.name);
+			if (items.length === 0) {
+				$scope.result = "Please enter data first";
+				document.getElementById('txtResult').style.color = '#ff2400';
+				document.getElementById('txtName').style.borderColor = '#ff2400';
+			} else if (items.length > 3) {
+				$scope.result = "Too much!";
+				document.getElementById('txtResult').style.color = '#05b626';
+				document.getElementById('txtName').style.borderColor = '#05b626';
+			} else {
+				$scope.result = "Enjoy";
+				document.getElementById('txtResult').style.color = '#05b626';
+				document.getElementById('txtName').style.borderColor = '#05b626';
+			};
+			$scope.message = $scope.result;
+		}
+		$scope.getItems = function(item) {
+			if (typeof item !== "string" || item === "") {
+				return [];
+			}
+			return item.split(",");
+		}
+	}
+})();
