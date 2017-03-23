df0205187f8f33bb9aba6b3bf15582b11f632234
Duoc-Do <duocdo.it@gmail.com>
2017-03-23 17:06:00 +0700

    updated not access directly to the DOM

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -1,13 +1,13 @@
 ! function() {
 	"use strict";
 
-	function e(e, t) {
-		e.name = "", e.message = "", e.result = "", e.btnCheck = function() {
-			var t = e.getItems(e.name);
-			0 === t.length ? (e.result = "Please enter data first", document.getElementById("txtResult").style.color = "#ff2400", document.getElementById("txtName").style.borderColor = "#ff2400") : t.length > 3 ? (e.result = "Too much!", document.getElementById("txtResult").style.color = "#05b626", document.getElementById("txtName").style.borderColor = "#05b626") : (e.result = "Enjoy", document.getElementById("txtResult").style.color = "#05b626", document.getElementById("txtName").style.borderColor = "#05b626"), e.message = e.result
-		}, e.getItems = function(e) {
-			return "string" != typeof e || "" === e ? [] : e.split(",")
+	function t(t, e) {
+		t.name = "", t.message = "", t.result = "", t.btnCheck = function() {
+			var e = t.getItems(t.name);
+			0 === e.length ? (t.result = "Please enter data first!", t.customClass = "txt-red", t.borderStatus = "red-status") : e.length > 3 ? (t.result = "Too much!", t.customClass = "txt-green", t.borderStatus = "green-status") : (t.result = "Enjoy!", t.customClass = "txt-green", t.borderStatus = "green-status"), t.message = t.result
+		}, t.getItems = function(t) {
+			return "string" != typeof t || "" === t ? [] : t.split(",")
 		}
 	}
-	angular.module("LunchCheck", []).controller("LunchCheckController", e), e.$inject = ["$scope", "$filter"]
+	angular.module("LunchCheck", []).controller("LunchCheckController", t), t.$inject = ["$scope", "$filter"]
 }();
